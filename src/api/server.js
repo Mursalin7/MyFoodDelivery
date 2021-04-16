3a48812a837b0d8bd9ecc2f50a1f190ee07fc164
Mursalin7 <mursalinhq1@gmail.com>
2021-04-17 02:53:49 +0530

    fix: added production server changes

--- src/api/server.js
+++ src/api/server.js
@@ -20,4 +20,14 @@ app.post('/api/orders', (req, res) => {
   );
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../../../public')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../../public', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
